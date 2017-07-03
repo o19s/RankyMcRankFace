670ba231da8ad6648603093ee22d12384f703e82
Doug Turnbull <softwaredoug@gmail.com>
2017-07-03 12:58:41 -0400

    Add impact reporting for random forest

--- test/ciir/umass/edu/eval/FeatureStrengthTest.java
+++ test/ciir/umass/edu/eval/FeatureStrengthTest.java
@@ -25,18 +25,18 @@ public class FeatureStrengthTest extends RankyTest {
         _rankFile = new TmpFile();
     }
 
-    String runLambdaMart(boolean captureStdOut) {
+    String runAndCaptureRanker(int rnum, boolean captureStdOut) {
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         if (captureStdOut) {
             System.setOut(new PrintStream(outContent));
         }
 
-        testRanker(_dataFile, _modelFile, _rankFile, 6, "ndcg@4");
+        testRanker(_dataFile, _modelFile, _rankFile, rnum, "ndcg@4");
         return outContent.toString();
     }
 
     @Test
-    public void testFeatureStrength() throws IOException {
+    public void testLambdaMARTFeatureStrength() throws IOException {
         _dataFile = new TmpFile();
         try (PrintWriter out = _dataFile.getWriter()) {
             out.println("## LambdaMART ");
@@ -49,7 +49,28 @@ public class FeatureStrengthTest extends RankyTest {
             out.println("0 qid:1 1:0.0 2:1    # P7");
             out.println("0 qid:1 1:0.0 2:1    # P8");
         }
-        String stdOut = runLambdaMart(true);
+        String stdOut = runAndCaptureRanker(6, true);
+        String ftrImpacts = stdOut.substring(stdOut.indexOf("FEATURE IMPACTS"));
+        // we expect "Feature 1" to be a higher impact than feature 2, ensure it comes first
+        assert(ftrImpacts.indexOf("Feature 1") < ftrImpacts.indexOf("Feature 2"));
+
+    }
+
+    @Test
+    public void testRandomForestFeatureStrength() throws IOException {
+        _dataFile = new TmpFile();
+        try (PrintWriter out = _dataFile.getWriter()) {
+            out.println("## LambdaMART ");
+            out.println("4 qid:1 1:100.0 2:1  # P1");
+            out.println("4 qid:1 1:100.0 2:1  # P2 ");
+            out.println("4 qid:1 1:100.0 2:1  # P3");
+            out.println("4 qid:1 1:100.0 2:1  # P4");
+            out.println("0 qid:1 1:0.0 2:1    # P5");
+            out.println("0 qid:1 1:0.0 2:1    # P6");
+            out.println("0 qid:1 1:0.0 2:1    # P7");
+            out.println("0 qid:1 1:0.0 2:1    # P8");
+        }
+        String stdOut = runAndCaptureRanker(8, true);
         String ftrImpacts = stdOut.substring(stdOut.indexOf("FEATURE IMPACTS"));
         // we expect "Feature 1" to be a higher impact than feature 2, ensure it comes first
         assert(ftrImpacts.indexOf("Feature 1") < ftrImpacts.indexOf("Feature 2"));
